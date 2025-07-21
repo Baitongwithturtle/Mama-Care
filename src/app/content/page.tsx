97b5578ae8e3ac646d1dc5ad8fa079f4930bc17f
banyaphon <92526947+bypkt-bk@users.noreply.github.com>
2025-07-22 02:48:29 +0700

    Add Google SVG icon and implement MenuBar

--- src/app/content/page.tsx
+++ src/app/content/page.tsx
@@ -1,41 +1,45 @@
+import MenuBar from "@/component/menu_bar";
 import React from "react";
 
 const page = () => {
   return (
     <div className="mobile flex flex-col items-center justify-between relative">
-  {/* Section: Top Content */}
-  <div className="flex flex-col items-center h-full gap-10">
-    {/* Top Image */}
-    <img src="assets/5.webp" alt="frame" />
+      {/* Section: Top Content */}
+      <div className="flex flex-col items-center h-full gap-20">
+        {/* Top Image */}
+        <img src="assets/5.webp" alt="frame" />
+        {/* <div className="w-full flex items-end justify-end px-5">
+          <MenuBar />
+        </div> */}
 
-    {/* Content Box */}
-    <div className="flex flex-col items-center justify-start w-full min-h-full px-20 gap-4">
-      {/* Header Section */}
-      <div className="bg-[#62ABA4] rounded-[8px] px-8 shadow-lg text-white text-center">
-        <h1 className="text-3xl font-baloo text-black">Content</h1>
-        <p className="font-sarabun">ความรู้ทั่วไป</p>
+        {/* Content Box */}
+        <div className="flex flex-col items-center justify-start w-full min-h-full px-20 gap-4">
+          {/* Header Section */}
+          <div className="bg-[#62ABA4] rounded-[8px] px-8 shadow-lg text-white text-center">
+            <h1 className="text-3xl font-baloo text-black">Content</h1>
+            <p className="font-sarabun">ความรู้ทั่วไป</p>
+          </div>
+
+          {/* Buttons */}
+          <button className="bg-[#AADAD5] font-sarabun w-full min-h-[40px] px-8 border border-white text-xl">
+            สาเหตุของการติดเชื้อ
+          </button>
+          <button className="bg-[#AADAD5] font-sarabun w-full min-h-[40px] px-8 border border-white text-xl">
+            อาการและอาการแสดงจากการติดเชื้อ
+          </button>
+          <button className="bg-[#AADAD5] font-sarabun w-full min-h-[40px] px-8 border border-white text-xl">
+            การป้องกันการติดเชื้อของแผล
+          </button>
+          <button className="bg-[#AADAD5] font-sarabun w-full min-h-[40px] px-8 py-2 border border-white text-xl">
+            การดูแลแผลผ่าตัด
+          </button>
+        </div>
       </div>
 
-      {/* Buttons */}
-      <button className="bg-[#AADAD5] font-sarabun w-full min-h-[40px] px-8 border border-white text-xl">
-        สาเหตุของการติดเชื้อ
-      </button>
-      <button className="bg-[#AADAD5] font-sarabun w-full min-h-[40px] px-8 border border-white text-xl">
-        อาการและอาการแสดงจากการติดเชื้อ
-      </button>
-      <button className="bg-[#AADAD5] font-sarabun w-full min-h-[40px] px-8 border border-white text-xl">
-        การป้องกันการติดเชื้อของแผล
-      </button>
-      <button className="bg-[#AADAD5] font-sarabun w-full min-h-[40px] px-8 py-2 border border-white text-xl">
-        การดูแลแผลผ่าตัด
-      </button>
+      {/* Bottom Image */}
+
+      <img src="assets/1.webp" alt="frame" />
     </div>
-  </div>
-
-  {/* Bottom Image */}
-  <img src="assets/1.webp" alt="frame" />
-</div>
-
   );
 };
 
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,23 +1,31 @@
+"use client";
 import React from 'react'
 import Image from 'next/image'
+import { useRouter } from "next/navigation";
+
 const page = () => {
+  const router = useRouter();
+
+  function handleLogin() {
+    router.push("/content");
+  }
   return (
-    <div className='bg-[#FCF9DA] flex-col mobile flex gap-10 items-center'>
+    <div className='bg-[#FCF9DA] flex-col mobile flex gap-10 items-center justify-between'>
     <img alt='curve' src="assets/1.webp" className='rotate-180'></img>
     <img alt='center mama care' src="assets/4.webp" width={350} height={0}></img>
 
-    <div>
-    <h1 className='text-3xl font-bold text black flex flex-col items-center'>Login Here</h1>
-    <h2 className='text-lg font-semibold text-black flex flex-col items-center'>Welcome to planner</h2>
+    <div className='flex flex-col items-center gap-2'>
+    <h1 className='text-[52px] leading-[52px] font-bold font-baloo'>Login Here</h1>
+    <h2 className='text-[24px] leading-[24px] font-semibold'>Welcome to planner</h2>
     </div>
 
-    <button className='flex items-center gap-2 px-20 py-2 bg-white border border-[#DD6774] rounded-full shadow hover:bg-gray-100 transition'>
-      <img alt="google icon" src="assets/5.webp" width={20} height={20}></img>
-      <span className='text-nowrap'>Login with Google</span>
+    <button onClick={handleLogin} className='flex items-center justify-center gap-2 px-20 py-2 bg-white border border-[#DD6774] rounded-full shadow hover:bg-gray-100 transition'>
+      <img alt="google icon" src="assets/google.svg" width={20} height={20}></img>
+      <p className='text-nowrap font-baloo text-[16px] leading-[16px]'>Login with Google</p>
     </button>
 
-    <img alt= "foot" src= "assets/3.webp" className="mt-12" width={200} ></img>
-     <img alt='curve' src="assets/1.webp"></img>
+    <img alt= "foot" src= "assets/3.webp"  width={200} />
+     <img alt='curve' src="assets/1.webp" />
     </div>
   )
 }
