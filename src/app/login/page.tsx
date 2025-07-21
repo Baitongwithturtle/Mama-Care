0d10ec7026cc6148ad3dd12739faa9cc45b879db
baitong <kanyanat1093@gmail.com>
2025-07-21 23:05:47 +0700

    layout

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Image from 'next/image'
+const page = () => {
+  return (
+    <div className='mobile'><img alt='mama care' src="assets/4.webp" width={300} height={0}></img></div>
+  )
+}
+
+export default page
